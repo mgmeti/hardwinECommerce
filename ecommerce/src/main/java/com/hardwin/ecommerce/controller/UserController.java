2259fa8e7466e608e9f9df664469733e5c756033
mgmeti <mgmeti18@gmail.com>
2024-11-18 15:51:50 +0530

    Defined user controller methods

--- ecommerce/src/main/java/com/hardwin/ecommerce/controller/UserController.java
+++ ecommerce/src/main/java/com/hardwin/ecommerce/controller/UserController.java
@@ -1,9 +1,19 @@
 package com.hardwin.ecommerce.controller;
 
+import com.hardwin.ecommerce.dto.UserDto;
+import com.hardwin.ecommerce.entity.User;
+import com.hardwin.ecommerce.exception.AlreadyExistsException;
+import com.hardwin.ecommerce.exception.ResourceNotFoundException;
+import com.hardwin.ecommerce.request.CreateUserRequest;
+import com.hardwin.ecommerce.request.UserUpdateRequest;
+import com.hardwin.ecommerce.response.ApiResponse;
 import com.hardwin.ecommerce.service.IUserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import static org.springframework.http.HttpStatus.CONFLICT;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 @RequiredArgsConstructor
 @RestController
@@ -12,7 +22,46 @@ public class UserController {
 
     private final IUserService userService;
 
+    @GetMapping("/{userId}/user")
+    public ResponseEntity<ApiResponse> getUserById(@PathVariable Long userId){
+        try{
+            User user = userService.getUserById(userId);
+            UserDto userDto = userService.convertUserToDto(user);
+            return ResponseEntity.ok(new ApiResponse("Success", userDto));
+        } catch (ResourceNotFoundException rnfe){
+            return ResponseEntity.status(NOT_FOUND).body(new ApiResponse(rnfe.getMessage(), null));
+        }
+    }
 
+    @PostMapping("/add")
+    public ResponseEntity<ApiResponse> createUser(@RequestBody CreateUserRequest request){
+        try {
+            User user = userService.createUser(request);
+            UserDto userDto = userService.convertUserToDto(user);
+            return ResponseEntity.ok(new ApiResponse("Create User Success!", userDto));
+        } catch (AlreadyExistsException aee) {
+            return ResponseEntity.status(CONFLICT).body(new ApiResponse(aee.getMessage(), null));
+        }
 
+    }
+    @PutMapping("/{userId}/update")
+    public ResponseEntity<ApiResponse> updateUser(@RequestBody UserUpdateRequest request, @PathVariable Long userId) {
+        try {
+            User user = userService.updateUser(request, userId);
+            UserDto userDto = userService.convertUserToDto(user);
+            return ResponseEntity.ok(new ApiResponse("Update User Success!", userDto));
+        } catch (ResourceNotFoundException e) {
+            return ResponseEntity.status(NOT_FOUND).body(new ApiResponse(e.getMessage(), null));
+        }
+    }
 
+    @DeleteMapping("/{userId}/delete")
+    public ResponseEntity<ApiResponse> deleteUser(@PathVariable Long userId) {
+        try {
+            userService.deleteUser(userId);
+            return ResponseEntity.ok(new ApiResponse("Delete User Success!", null));
+        } catch (ResourceNotFoundException e) {
+            return ResponseEntity.status(NOT_FOUND).body(new ApiResponse(e.getMessage(), null));
+        }
+    }
 }
