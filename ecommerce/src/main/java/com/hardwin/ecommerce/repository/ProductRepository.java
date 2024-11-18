0a7b1c6acf01a9b2ef029f7be2f48fb0ff9f29a6
mgmeti <mgmeti18@gmail.com>
2024-11-18 17:03:25 +0530

    Defined product controller and service

--- ecommerce/src/main/java/com/hardwin/ecommerce/repository/ProductRepository.java
+++ ecommerce/src/main/java/com/hardwin/ecommerce/repository/ProductRepository.java
@@ -9,7 +9,8 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
     List<Product> findByCategoryName(String category);
     List<Product> findByBrand(String category);
     List<Product> findByName(String category);
-    List<Product> findByCategoryNameAndBrand(String category);
-    List<Product> findByBrandAndName(String category);
+    List<Product> findByCategoryNameAndBrand(String category, String name);
+    List<Product> findByBrandAndName(String brand, String name);
 
+    Long countByBrandAndName(String brand, String name);
 }
---
+++ ecommerce/src/main/java/com/hardwin/ecommerce/service/ProductService.java
@@ -0,0 +1,126 @@
+package com.hardwin.ecommerce.service;
+
+import com.hardwin.ecommerce.dto.ProductDto;
+import com.hardwin.ecommerce.entity.Category;
+import com.hardwin.ecommerce.entity.Product;
+import com.hardwin.ecommerce.exception.ResourceNotFoundException;
+import com.hardwin.ecommerce.repository.CategoryRepository;
+import com.hardwin.ecommerce.repository.ProductRepository;
+import com.hardwin.ecommerce.request.AddProductRequest;
+import com.hardwin.ecommerce.request.ProductUpdateRequest;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class ProductService implements IProductService{
+    private final ProductRepository productRepository;
+    private final CategoryRepository categoryRepository;
+    private final ModelMapper modelMapper;
+
+    @Override
+    public Product addProduct(AddProductRequest request) {
+
+        Category category = Optional.ofNullable(categoryRepository.findByName(request.getCategory().getName()))
+                .orElseGet(() -> {
+                    Category newCategory = new Category(request.getCategory().getName());
+                    return categoryRepository.save(newCategory);
+                });
+        request.setCategory(category);
+        return productRepository.save(createProduct(request, category));
+    }
+
+    private Product createProduct(AddProductRequest request, Category category) {
+        return  new Product(
+                request.getName(),
+                request.getBrand(),
+                request.getPrice(),
+                request.getInventory(),
+                request.getDescription(),
+                category
+        );
+    }
+
+    @Override
+    public Product getProductById(Long id) {
+        return productRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Product not found"));
+    }
+
+    @Override
+    public void deleteProductById(Long id) {
+        productRepository.findById(id)
+                .ifPresentOrElse(productRepository::delete,
+                        () -> {throw new ResourceNotFoundException("Product not found");});
+    }
+
+    @Override
+    public Product updateProduct(ProductUpdateRequest request, Long productId) {
+        return productRepository.findById(productId)
+                .map(existingProduct -> updateExistingProduct(existingProduct, request))
+                .map(productRepository :: save)
+                .orElseThrow(() -> new ResourceNotFoundException("Product not found"));
+    }
+
+    private Product updateExistingProduct(Product existingProduct, ProductUpdateRequest request) {
+        existingProduct.setName(request.getName());
+        existingProduct.setBrand(request.getBrand());
+        existingProduct.setPrice(request.getPrice());
+        existingProduct.setInventory(request.getInventory());
+        existingProduct.setDescription(request.getDescription());
+
+        Category category = categoryRepository.findByName(request.getCategory().getName());
+        existingProduct.setCategory(category);
+        return existingProduct;
+    }
+
+
+    @Override
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    @Override
+    public List<Product> getProductsByCategory(String category) {
+        return productRepository.findByCategoryName(category);
+    }
+
+    @Override
+    public List<Product> getProductsByBrand(String brand) {
+        return productRepository.findByBrand(brand);
+    }
+
+    @Override
+    public List<Product> getProductsByCategoryAndBrand(String category, String brand) {
+        return productRepository.findByCategoryNameAndBrand(category, brand);
+    }
+
+    @Override
+    public List<Product> getProductsByName(String name) {
+        return productRepository.findByName(name);
+    }
+
+    @Override
+    public List<Product> getProductsByBrandAndName(String brand, String name) {
+        return productRepository.findByBrandAndName(brand, name);
+    }
+
+    @Override
+    public Long countProductsByBrandAndName(String brand, String name) {
+        return productRepository.countByBrandAndName(brand, name);
+    }
+
+    @Override
+    public List<ProductDto> getConvertedProducts(List<Product> products) {
+        return products.stream().map(this::convertToDto).toList();
+    }
+
+    @Override
+    public ProductDto convertToDto(Product product) {
+       return modelMapper.map(product, ProductDto.class);
+    }
+}
